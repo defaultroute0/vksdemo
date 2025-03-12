7eccbfc3c63186edd3fae557f750eeb5e3f44165
Ryan Douglas <67136481+defaultroute0@users.noreply.github.com>
2025-03-12 11:37:27 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,3 +10,6 @@ kubectl apply -f .\sandercluster01.yaml
 kubectl-vsphere login
 kubectl-vsphere login --server=https://10.80.0.2 --insecure-skip-tls-verify --vsphere-username administrator@vsphere.local
 kubectl-vsphere login --server=https://10.80.0.2 --insecure-skip-tls-verify --tanzu-kubernetes-cluster-name ryan-cluster01 --vsphere-username administrator@vsphere.local 
+---
+Deploying into guest cluster need to label the ns to relax security
+https://techdocs.broadcom.com/us/en/vmware-cis/vsphere/vsphere-supervisor/8-0/using-tkg-service-with-vsphere-supervisor/managing-security-for-tkg-service-clusters/configure-psa-for-tkr-1-25-and-later.html
