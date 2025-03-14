f64f4ce4543d1f45c6b0dbac0cdb5746526630e8
Ryan Douglas <67136481+defaultroute0@users.noreply.github.com>
2025-03-14 14:22:06 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ kubectl describe ns ns01
 kubectl get sc
 kubectl get tkr
 kubectl get virtualmachineclass
-kubectl apply -f .\ryancluster01.yaml
-kubectl apply -f .\sandercluster01.yaml
+kubectl apply -f .\ryancluster01.yaml -n ns01
+kubectl apply -f .\sandercluster01.yaml -n ns01
 kubectl-vsphere login
 kubectl-vsphere login --server=https://10.80.0.2 --insecure-skip-tls-verify --vsphere-username administrator@vsphere.local
 kubectl-vsphere login --server=https://10.80.0.2 --insecure-skip-tls-verify --tanzu-kubernetes-cluster-name ryan-cluster01 --vsphere-username administrator@vsphere.local 
